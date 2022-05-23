0e40627934ee91e6525866dd710d510ea3497dc5
nyk <nykcasseano13@gmail.com>
2022-05-23 20:55:44 -0300

    Projeto de medidor de  coraçãoo  inteligente

---
+++ medido.py
@@ -0,0 +1,10 @@
+batimentos = 130
+
+muito_baixo = batimentos < 60
+muito_alto = batimento > 120
+
+print("Batimentos muito baixos:")
+print(muito_baixo)
+
+print("Batimentos alto")
+print(muito_alto)
